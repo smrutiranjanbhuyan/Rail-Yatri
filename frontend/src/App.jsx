d237e9169fa461b3c7242794b835269dd205790d
smrutiranjanbhuyan <smruti452004@gmai.com>
2024-08-24 22:41:14 +0530

    React+telwind conf

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-600'>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, labore officiis voluptas exercitationem repellendus aut adipisci, ipsa non ipsum aliquam voluptatibus eius, ratione nulla repellat. Quibusdam aperiam doloribus odio ex!
+    </div>
+  )
+}
+
+export default App
